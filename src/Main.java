7476e567861adb38ba976f6387fefe4d9f42c8a0
Ben Jonas Kreuz <adivius@chaos-siegen.de>
2022-12-13 16:55:02 +0100

    Added colors

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,93 @@
-import sum.kern.Bildschirm;
-import sum.kern.Buntstift;
-import sum.kern.Maus;
-import sum.kern.Tastatur;
+import sum.kern.*;
+
+import java.awt.*;
 import java.util.Random;
+/**
+ * @Author: Adivius
+ * @Version: 2.1
+ *
+ * School projekt in Java with SuM
+ * */
 
 public class Main {
-
-    private final int SCREEN_HEIGHT = 400, SCREEN_WIGHT = 600, FALL_SPEED = 2, SHOOT_SPEED = 6, ROTATION_SPEED = 2;
-    public int targetX, targetY;
+    /**
+     * Defining all necessary vars and consts
+     * */
+    private final int SCREEN_HEIGHT = 600, SCREEN_WIGHT = 800, FALL_SPEED = 6, SHOOT_SPEED = 9, ROTATION_SPEED = 2, GAME_DELAY = 3;
+    public int targetX = 0, targetY = 0, lastPoint = 0;
     public Bildschirm screen = new Bildschirm(SCREEN_WIGHT, SCREEN_HEIGHT);
     public Buntstift pen = new Buntstift();
     public Maus mouse = new Maus();
     public Tastatur key = new Tastatur();
-
+    /**
+     * Main function for starting to programm to create a Main object
+     * */
     public static void main(String[] args) {
         new Main();
     }
-
+    /**
+     * Constructor to run the game code
+     * */
     public Main() {
-        screen.setResizable(false);
-        sleep(5);
-        drawTarget();
-        pen.bewegeBis(100, 10);
-
-        while (!mouse.istGedrueckt()) {
-            pen.bewegeBis(pen.hPosition(), (pen.vPosition() + FALL_SPEED) % SCREEN_HEIGHT);
-            drawDart();
-        }
-
-        int rotation = 0;
-        while (mouse.istGedrueckt()) {
-            drawDart();
-            rotation = rotation <= -80 ? 80 : rotation - ROTATION_SPEED;
-            pen.dreheBis(rotation);
-        }
-
-        while (pen.hPosition() + 30 <= targetX && pen.hPosition() >= 0) {
-            drawDart();
-            pen.bewegeUm(SHOOT_SPEED);
-            if (key.wurdeGedrueckt()) {
-                switch (key.zeichen()) {
-                    case 'w' -> pen.dreheUm(ROTATION_SPEED / 1.5);
-                    case 's' -> pen.dreheUm(ROTATION_SPEED / -1.5);
-                }
-                key.weiter();
+        while (true){
+            screen.setResizable(false);
+            sleep(5);
+            drawTarget();
+            pen.bewegeBis(20, 10);
+            while (!mouse.istGedrueckt()) {
+                pen.bewegeBis(pen.hPosition(), (pen.vPosition() + FALL_SPEED) % SCREEN_HEIGHT);
+                drawDart(true);
             }
+            int rotation = 0;
+            while (mouse.istGedrueckt()) {
+                drawDart(true);
+                rotation = rotation <= -80 ? 80 : rotation - ROTATION_SPEED;
+                pen.dreheBis(rotation);
+            }
+            while (pen.hPosition() + 30 <= targetX && pen.hPosition() >= 0) {
+                drawDart(true);
+                pen.bewegeUm(SHOOT_SPEED);
+                if (key.wurdeGedrueckt()) {
+                    switch (key.zeichen()) {
+                        case 'w' -> pen.dreheUm(ROTATION_SPEED / 1.5);
+                        case 's' -> pen.dreheUm(ROTATION_SPEED / -1.5);
+                    }
+                    key.weiter();
+                }
+            }
+            pen.bewegeUm(30);
+            boolean win = pen.vPosition() <= targetY + 35 && pen.vPosition() >= targetY - 35;
+            int points = win ? 35 - Math.abs((int) (targetY - pen.vPosition())) : 0;
+            int percentage = (points * 10) / 35;
+            pen.bewegeUm(-30);
+            drawTarget();
+            writeMid(win ? "Win" : "Fail", percentage, lastPoint, win);
+            lastPoint = percentage;
+            drawDart(false);
+            end();
         }
-        pen.bewegeUm(30);
-        boolean win = pen.vPosition() <= targetY + 35 && pen.vPosition() >= targetY - 35;
-        int points = win ? 35 - Math.abs((int)(targetY - pen.vPosition())) : 0;
-        int percentage = (points * 100) / 35;
-        pen.bewegeUm(-30);
-        drawTarget();
-        end();
-        writeMid(win ? "Win" : "Fail", percentage);
     }
-
-    public void drawDart() {
+    /**
+     * Function for drawing dart every tick
+     * */
+    public void drawDart(boolean delete) {
         pen.runter();
         pen.bewegeUm(30);
         pen.zeichneKreis(3);
         pen.hoch();
-        sleep(20);
-        pen.radiere();
-        pen.runter();
-        pen.zeichneKreis(3);
-        pen.bewegeUm(-30);
-        pen.hoch();
-        pen.normal();
+        if (delete){
+            sleep(20);
+            pen.radiere();
+            pen.runter();
+            pen.zeichneKreis(3);
+            pen.bewegeUm(-30);
+            pen.hoch();
+            pen.normal();
+        }
     }
-
+    /**
+     * Function for drawing target at start and end of the game
+     * */
     public void drawTarget() {
         Buntstift targetPen = new Buntstift();
         if (targetX == 0) {
@@ -80,25 +98,32 @@ public class Main {
         targetPen.zeichneKreis(35);
         targetPen.zeichneKreis(25);
         targetPen.zeichneKreis(15);
+        targetPen.setzeFarbe(new Color(255, 0, 0));
         targetPen.zeichneKreis(5);
     }
-
+    /**
+     * Function for resetting the game
+     * */
     public void end() {
-        pen.normal();
-        pen.runter();
-        pen.bewegeUm(30);
-        pen.zeichneKreis(3);
-        pen.hoch();
+        targetX = 0;
+        targetY = 0;
+        sleep(GAME_DELAY * 1000);
+        screen.loescheAlles();
     }
-
-    public void writeMid(String text, int points) {
-        text = text + ": " + points + " p.";
+    /**
+     * Writes the game status at the end of the game
+     * */
+    public void writeMid(String text, int points, int lastPoint, boolean win) {
+        text = text + ": " + points + "$, last game: " + lastPoint + "$";
         Buntstift writePen = new Buntstift();
-        writePen.bewegeBis(SCREEN_WIGHT / 2.0, SCREEN_HEIGHT / 2.0);
+        writePen.setzeFarbe(win ? Farbe.GRUEN : Farbe.ROT);
+        writePen.bewegeBis((SCREEN_WIGHT / 2.0) - 80, SCREEN_HEIGHT / 2.0);
         writePen.setzeSchriftGroesse(20);
         writePen.schreibeText(text);
     }
-
+    /**
+     * Function for delaying the Thread
+     * */
     public void sleep(int t){
         try {
             Thread.sleep(t);
@@ -106,6 +131,4 @@ public class Main {
             System.err.println(e.getMessage());
         }
     }
-
-
 }
