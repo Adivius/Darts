eb1b0d9f800553434d635530970dd7793eb84bce
Adivius <adivius@chaos-siegen.de>
2022-12-14 23:25:00 +0100

    Added color && changed Dart behaviour

--- src/Main.java
+++ src/Main.java
@@ -2,47 +2,56 @@ import sum.kern.*;
 
 import java.awt.*;
 import java.util.Random;
+
 /**
  * @Author: Adivius
  * @Version: 2.1
- *
+ * <p>
  * School projekt in Java with SuM
- * */
+ */
 
 public class Main {
     /**
      * Defining all necessary vars and consts
-     * */
+     */
     private final int SCREEN_HEIGHT = 600, SCREEN_WIGHT = 800, FALL_SPEED = 6, SHOOT_SPEED = 9, ROTATION_SPEED = 2, GAME_DELAY = 3;
     public int targetX = 0, targetY = 0, lastPoint = 0;
     public Bildschirm screen = new Bildschirm(SCREEN_WIGHT, SCREEN_HEIGHT);
     public Buntstift pen = new Buntstift();
     public Maus mouse = new Maus();
     public Tastatur key = new Tastatur();
+
     /**
      * Main function for starting to programm to create a Main object
-     * */
+     */
     public static void main(String[] args) {
         new Main();
     }
+
     /**
      * Constructor to run the game code
-     * */
+     */
     public Main() {
-        while (true){
+        while (true) {
             screen.setResizable(false);
             sleep(5);
             drawTarget();
             pen.bewegeBis(20, 10);
+            boolean target = false;
             while (!mouse.istGedrueckt()) {
                 pen.bewegeBis(pen.hPosition(), (pen.vPosition() + FALL_SPEED) % SCREEN_HEIGHT);
                 drawDart(true);
+                if (key.wurdeGedrueckt() && key.zeichen() == 'x') target = true;
             }
             int rotation = 0;
             while (mouse.istGedrueckt()) {
+                if (target) {
+                    pen.dreheZu(targetX, targetY);
+                    break;
+                }
                 drawDart(true);
                 rotation = rotation <= -80 ? 80 : rotation - ROTATION_SPEED;
-                pen.dreheBis(rotation);
+                    pen.dreheBis(rotation);
             }
             while (pen.hPosition() + 30 <= targetX && pen.hPosition() >= 0) {
                 drawDart(true);
@@ -67,15 +76,16 @@ public class Main {
             end();
         }
     }
+
     /**
      * Function for drawing dart every tick
-     * */
+     */
     public void drawDart(boolean delete) {
         pen.runter();
         pen.bewegeUm(30);
         pen.zeichneKreis(3);
         pen.hoch();
-        if (delete){
+        if (delete) {
             sleep(20);
             pen.radiere();
             pen.runter();
@@ -85,9 +95,10 @@ public class Main {
             pen.normal();
         }
     }
+
     /**
      * Function for drawing target at start and end of the game
-     * */
+     */
     public void drawTarget() {
         Buntstift targetPen = new Buntstift();
         if (targetX == 0) {
@@ -101,18 +112,20 @@ public class Main {
         targetPen.setzeFarbe(new Color(255, 0, 0));
         targetPen.zeichneKreis(5);
     }
+
     /**
      * Function for resetting the game
-     * */
+     */
     public void end() {
         targetX = 0;
         targetY = 0;
         sleep(GAME_DELAY * 1000);
         screen.loescheAlles();
     }
+
     /**
      * Writes the game status at the end of the game
-     * */
+     */
     public void writeMid(String text, int points, int lastPoint, boolean win) {
         text = text + ": " + points + "$, last game: " + lastPoint + "$";
         Buntstift writePen = new Buntstift();
@@ -121,10 +134,11 @@ public class Main {
         writePen.setzeSchriftGroesse(20);
         writePen.schreibeText(text);
     }
+
     /**
      * Function for delaying the Thread
-     * */
-    public void sleep(int t){
+     */
+    public void sleep(int t) {
         try {
             Thread.sleep(t);
         } catch (InterruptedException e) {
